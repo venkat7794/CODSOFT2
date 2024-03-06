9986c5ec3354c3ec57a40a20e3bf66718bcdb4ad
VENKATA SREEKANTH <161672611+venkat7794@users.noreply.github.com>
2024-03-06 10:00:14 +0530

    Add files via upload

---
+++ GradeCalculator.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class GradeCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter the number of subjects: ");
+        int numOfSubjects = scanner.nextInt();
+        
+        int totalMarks = 0;
+        for (int i = 1; i <= numOfSubjects; i++) {
+            System.out.print("Enter marks obtained in subject " + i + ": ");
+            int marks = scanner.nextInt();
+            totalMarks += marks;
+        }
+        
+        double averagePercentage = (double) totalMarks / numOfSubjects;
+        
+        System.out.println("Total Marks: " + totalMarks);
+        System.out.println("Average Percentage: " + averagePercentage + "%");
+        
+        char grade = calculateGrade(averagePercentage);
+        System.out.println("Grade: " + grade);
+        
+        scanner.close();
+    }
+    
+    public static char calculateGrade(double averagePercentage) {
+        if (averagePercentage >= 90) {
+            return 'A';
+        } else if (averagePercentage >= 80) {
+            return 'B';
+        } else if (averagePercentage >= 70) {
+            return 'C';
+        } else if (averagePercentage >= 60) {
+            return 'D';
+        } else if (averagePercentage >= 50) {
+            return 'E';
+        } else {
+            return 'F';
+        }
+    }
+}
